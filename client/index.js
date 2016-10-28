faea9e2168092f8af46b5d704977cce3c0c465bf
Nhan Nguyen <nxtnhan@gmail.com>
2016-10-28 11:57:37 +0900

    support material render from server

--- client/index.js
+++ client/index.js
@@ -2,7 +2,8 @@ const router = require('./router');
 
 // export main function for server side rendering
 global.main = router.renderToString;
-
+// global.navigator = { userAgent: 'all' }; 
+// global.navigator = { navigator: 'all' };
 // start app if it in the browser
 if(typeof window !== 'undefined') {
   // Start main application here
